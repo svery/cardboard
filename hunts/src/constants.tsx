058cd4e75ac595680fb4b8d994ac4989783b2b3a
Sy <svery@users.noreply.github.com>
2025-01-16 14:48:42 -0500

    Update constants.tsx

--- hunts/src/constants.tsx
+++ hunts/src/constants.tsx
@@ -1,8 +1,6 @@
 export const DEFAULT_TAG_COLOR = "primary";
 export const SELECTABLE_TAG_COLORS = [
-  { color: "primary", display: "Blue" },
   { color: "light", display: "White" },
-  { color: "info", display: "Teal (Location)" },
 ];
 
 export const SHEET_REDIRECT_BASE = "/puzzles/s";
